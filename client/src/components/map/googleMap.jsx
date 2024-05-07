ba5b56d455ef7e39d6d4ab444d55e8050a66043c
LeeShinwon <leeshin1130@gmail.com>
2024-05-07 13:38:33 +0900

    0507신원: google marker 수정

--- client/src/components/map/googleMap.jsx
+++ client/src/components/map/googleMap.jsx
@@ -4,60 +4,73 @@ import React, { useRef, useState, useEffect } from "react";
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 
+import googleMapID from 'src/config/googleMapId';
 import useNodeInfoStore from "src/store/nodeInfoStore";
 import useOverViewStore from 'src/store/overViewStore';
+
 
 const GoogleMap = ({title, subheader}) => {
   const ref = useRef(null);
   const [map, setMap] = useState();
 
   const { nodes } = useNodeInfoStore();
-  const { setSelectedLocation, selectedLocation } = useOverViewStore();
+  const { setSelectedLocation } = useOverViewStore();
 
   useEffect(() => {
-    
-    if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {
-        center: { lat: 36.1032734, lng: 129.3893488 },
-        zoom: 16,
-        styles: [
-          {
-            featureType: "all",
-            elementType: "labels",
-            stylers: [{ visibility: "off" }] // 모든 레이블을 숨깁니다.
-          },
+    async function initMap() {
+      if (!window.google) {
+        // Handle the case where Google Maps API is not loaded yet
+        console.error("Google Maps API is not loaded.");
+        return;
+      }
 
-        ]
-      }));
+      const { AdvancedMarkerElement } = await window.google.maps.importLibrary("marker");
+
+      if (ref.current && !map) {
+        const newMap = new window.google.maps.Map(ref.current, {
+          mapId: googleMapID,
+          center: { lat: 36.1032734, lng: 129.3893488 },
+          zoom: 16,
+          // styles: [{ featureType: "all", elementType: "labels", stylers: [{ visibility: "off" }] }]
+        });
+        setMap(newMap);
+      }
+
+      if (map) {
+        nodes.forEach((node) => {
+          const CustomNode = document.createElement('div');
+          CustomNode.className = 'customNode';
+          CustomNode.innerText = node.location;
+          CustomNode.style.cssText = `
+            width: 3.5em;
+            height: 3.5em;
+            border-radius: 50%;
+            text-align: center;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: rgb(125,157,219,0.7);
+            border: 2px solid #7D9DDB;
+          `;
+
+          const marker = new AdvancedMarkerElement({
+            map,
+            position: { lat: node.latitude, lng: node.longitude },
+            content: CustomNode,
+            title: node.location,
+          });
+
+          marker.addListener("click", () => {
+            console.log(node.location);
+            setSelectedLocation(node.location);
+          });
+        });
+      }
     }
 
-
-    // 노드 정보 가져오기
-    nodes.forEach((node) => {
-
-      const CustomNode = document.createElement('div');
-      CustomNode.className = 'customNode';
-      CustomNode.innerText = node.location;
-      CustomNode.style.border = `2px solid #7D9DDB`;
-      CustomNode.style.background = "rgb(125,157,219,0.7)";
-
-
-      const marker = new window.google.maps.Marker({
-        map,
-        position: { lat: node.latitude, lng: node.longitude },
-        content: CustomNode,
-        title: node.location,
-      });
-
-      marker.addListener("click", () => {
-        console.log(node.location);
-        setSelectedLocation(node.location);
-      });
-
-    });
-
-
-  }, [ref, map, nodes, setSelectedLocation, selectedLocation]);
+    initMap();
+  }, [map, nodes, setSelectedLocation]);  // Ensure correct dependencies
 
   return (
     <Card style={{ height: 500, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
@@ -65,13 +78,11 @@ const GoogleMap = ({title, subheader}) => {
       <div ref={ref} style={{ width: "100%", height: "82%" }}/>
     </Card>
   );
-
 };
 
 GoogleMap.propTypes = {
-  subheader: PropTypes.string,
   title: PropTypes.string,
+  subheader: PropTypes.string,
 };
-
 
 export default GoogleMap;
